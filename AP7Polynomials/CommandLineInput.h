e21acc3da9c6eb5dbacc906df3ae774148bd9b9a
Michał Świderski <michal.swiderski@wseiz.edu.pl>
2020-06-25 01:07:41 +0200

    Dodaj pliki projektów.

---
+++ AP7Polynomials/CommandLineInput.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class CommandLineInput
+{
+public:
+	static Equation* getEquationFromCommandLine(const std::string& command);
+	static Equation* sumEquations(Equation& first, Equation& second);
+	static Equation* substractEquations(Equation& first, Equation& second);
+	static Equation* derivative(Equation& first);
+	static double getDoubleFromCommand(const std::string& command);
+	static Equation* multiplyEquations(Equation& first, Equation& second);
+};
